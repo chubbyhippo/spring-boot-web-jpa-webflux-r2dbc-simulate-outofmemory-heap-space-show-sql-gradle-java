b46a92ae7252473db86c41f676ce23f642ecface
ch <mkmsacc@outlook.com>
2023-12-21 22:26:11 +0700

    Add Repository annotation to StudentRepository
    The Repository annotation has been added to the StudentRepository.java interface. This enables Spring to automatically detect our repository class and allows for more straightforward database access and exceptions handling.

--- src/main/java/com/example/demo/domain/StudentRepository.java
+++ src/main/java/com/example/demo/domain/StudentRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.domain;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface StudentRepository {
     List<Student> getStudents();
 }
