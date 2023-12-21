2fe550db01b838eabf9cb63ac2c041137fc0df8c
ch <mkmsacc@outlook.com>
2023-12-21 16:35:53 +0700

    test suite and updated the integration test to retrieve this new entity. Specifically, the "contextLoads" test was replaced with a new test "shouldGetStudents" that calls "/student" endpoint and expects a list of Student objects in the response.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.domain.Student;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void shouldGetStudents() {
+		webTestClient.get()
+				.uri(uriBuilder -> uriBuilder.path("/student")
+						.build())
+				.exchange()
+				.expectBodyList(Student.class);
 	}
 
 }
