ff51c5ae0a9305db41e3ad6e87b914858712fb09
ch <mkmsacc@outlook.com>
2023-12-21 16:34:52 +0700

    The provided commit message does not match with shown diff code snippet. Based on the given diff, the correct commit message would be:
    Add new entity Student.java
    
    Introduced a new entity, Student.java, in the domain package. This

---
+++ src/main/java/com/example/demo/domain/Student.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
