a5753996d40f7ce286b5c05379af7f5ea74a65a2
ch <mkmsacc@outlook.com>
2023-12-21 16:33:33 +0700

    The provided commit message doesn't match the diff. The correct commit message is:
    Create StudentService with StudentRepository dependency
    
    Created a new StudentService class and injected StudentRepository as a dependency. This structure introduces a clean separation between the service and repository layers, which improves the

---
+++ src/main/java/com/example/demo/application/StudentService.java
@@ -0,0 +1,12 @@
+package com.example.demo.application;
+
+import com.example.demo.domain.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+}
