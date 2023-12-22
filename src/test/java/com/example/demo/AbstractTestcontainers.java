a1f654f411587c8bad5ff74ee583b20e173e4fbb
ch <mkmsacc@outlook.com>
2023-12-22 11:16:57 +0700

    The provided commit message doesn't align with the code differences. The correct commit message based on the given "diff" should be:
    Add AbstractTestcontainers class for PostgreSQL setup
    
    Introduced an abstract class, AbstractTestcontainers, to manage PostgreSQL testcontainer configuration for integration testing. This allows dynamic property registration for PostgreSQL container properties such as JDBC URL, username, and password.

---
+++ src/test/java/com/example/demo/AbstractTestcontainers.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public abstract class AbstractTestcontainers {
+
+    @Container
+    public static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+
+    @DynamicPropertySource
+    public static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresql::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresql::getUsername);
+        registry.add("spring.datasource.password", postgresql::getPassword);
+    }
+}
