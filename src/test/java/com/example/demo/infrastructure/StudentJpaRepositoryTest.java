e599cf7505e719eda142395229fef6e04e8fe94b
ch <mkmsacc@outlook.com>
2023-12-22 11:17:09 +0700

    Add new test class StudentJpaRepositoryTest
    Created a new test class, StudentJpaRepositoryTest, for the StudentJpaRepository. This test class includes setup and teardown methods for a PostgreSQL instance and a single test method which checks for OutOfMemoryError.

---
+++ src/test/java/com/example/demo/infrastructure/StudentJpaRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.infrastructure;
+
+import com.example.demo.AbstractTestcontainers;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class StudentJpaRepositoryTest extends AbstractTestcontainers {
+
+    @Autowired
+    private StudentJpaRepository studentJpaRepository;
+
+    @BeforeAll
+    static void setUp() {
+        postgresql.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        postgresql.stop();
+    }
+
+    @Test
+    @DisplayName("should throw out of memory error")
+    void shouldThrowOutOfMemoryError() {
+        assertThatThrownBy(() -> studentJpaRepository.findAll())
+                .hasCauseInstanceOf(OutOfMemoryError.class)
+                .hasMessage("java.lang.OutOfMemoryError: Java heap space");
+    }
+}
