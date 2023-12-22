5db35a4551e64cbad86e3e8fb920d119f5da76a6
ch <mkmsacc@outlook.com>
2023-12-22 14:37:51 +0700

    Remove StudentRepository dependency from StudentJpaRepository
    StudentJpaRepository no longer extends the StudentRepository interface. The change simplifies the codebase by removing an unnecessary dependency. This should make the application easier to maintain and evolve, without sacrificing any needed functionality.

--- src/main/java/com/example/demo/infrastructure/StudentJpaRepository.java
+++ src/main/java/com/example/demo/infrastructure/StudentJpaRepository.java
@@ -1,8 +1,7 @@
 package com.example.demo.infrastructure;
 
 import com.example.demo.domain.Student;
-import com.example.demo.domain.StudentRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentJpaRepository extends JpaRepository<Student, Long>, StudentRepository {
+public interface StudentJpaRepository extends JpaRepository<Student, Long> {
 }
