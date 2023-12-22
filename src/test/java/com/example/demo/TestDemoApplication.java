7af584f8e4fe6f2625fd3ebffd02e448c6359ae9
ch <mkmsacc@outlook.com>
2023-12-22 15:50:15 +0700

    Refactor TestDemoApplication for Testcontainers integration
    Removed specific PostgreSQLContainer bean in favour of a more versatile Testcontainers configuration. The TestDemoApplication now imports and extends AbstractTestcontainers. This change simplifies the set up of container-based tests and improves the adaptability of the testing framework for different kinds of containers.

--- src/test/java/com/example/demo/TestDemoApplication.java
+++ src/test/java/com/example/demo/TestDemoApplication.java
@@ -2,22 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
+@ImportTestcontainers(AbstractTestcontainers.class)
 public class TestDemoApplication {
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.from(DemoApplication::main).with(TestDemoApplication.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(DemoApplication::main)
+                .with(TestDemoApplication.class)
+                .run(args);
+    }
 
 }
