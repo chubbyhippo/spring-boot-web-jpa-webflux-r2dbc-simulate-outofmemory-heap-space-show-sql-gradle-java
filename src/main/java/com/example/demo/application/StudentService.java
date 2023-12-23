e4797176892f048481fa77bc73b5090383a283fb
ch <mkmsacc@outlook.com>
2023-12-23 20:50:15 +0700

    Inject StudentJpaRepository using Qualifier annotation
    Added the @Qualifier annotation for specifying the StudentRepository bean in StudentService. This is used to prevent errors when multiple beans of the same type exist in the context. It ensures that the 'jpaRepository' bean is injected into the StudentRepository implementation.

--- src/main/java/com/example/demo/application/StudentService.java
+++ src/main/java/com/example/demo/application/StudentService.java
@@ -2,6 +2,8 @@ package com.example.demo.application;
 
 import com.example.demo.domain.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class StudentService {
+
+    @Qualifier("jpaRepository")
     private final StudentRepository studentRepository;
 
     public List<StudentDto> getStudents() {
