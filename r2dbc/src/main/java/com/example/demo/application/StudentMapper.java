7796f435efa1c58ea7a1cb0028491218c26519fc
ch <mkmsacc@outlook.com>
2023-12-25 11:07:26 +0700

    Prevent instantiation of StudentMapper utility class
    Added private constructor to the StudentMapper utility class to prevent its instantiation. This change throws an IllegalStateException if an attempt to instantiate the class is made, increasing the safety and integrity of the application's architecture.

--- r2dbc/src/main/java/com/example/demo/application/StudentMapper.java
+++ r2dbc/src/main/java/com/example/demo/application/StudentMapper.java
@@ -3,6 +3,9 @@ package com.example.demo.application;
 import com.example.demo.domain.Student;
 
 public class StudentMapper {
+    private StudentMapper() {
+        throw new IllegalStateException("Utility class");
+    }
     public static StudentDto toDto(Student student) {
         return StudentDto.builder()
                 .id(student.id())
