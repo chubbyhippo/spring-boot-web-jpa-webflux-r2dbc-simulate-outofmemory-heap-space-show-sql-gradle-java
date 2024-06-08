77ea702984c793a56543f9a3686458587b153008
ch <mkmsacc@outlook.com>
2024-06-08 23:33:36 +0700

    Refactor StudentJpaRepositoryTest to use TestcontainersConfiguration
    Removed AbstractTestcontainers dependency and replaced it with TestcontainersConfiguration in the StudentJpaRepositoryTest file. The change means the set up and tear down methods for postgresql are no longer needed in this class.

--- jpa/src/test/java/io/github/chubbyhippo/demo/infrastructure/StudentJpaRepositoryTest.java
+++ jpa/src/test/java/io/github/chubbyhippo/demo/infrastructure/StudentJpaRepositoryTest.java
@@ -1,32 +1,22 @@
 package io.github.chubbyhippo.demo.infrastructure;
 
-import io.github.chubbyhippo.demo.AbstractTestcontainers;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import io.github.chubbyhippo.demo.TestcontainersConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class StudentJpaRepositoryTest extends AbstractTestcontainers {
+@Import(TestcontainersConfiguration.class)
+class StudentJpaRepositoryTest {
 
     @Autowired
     private StudentJpaRepository studentJpaRepository;
-
-    @BeforeAll
-    static void setUp() {
-        postgresql.start();
-    }
-
-    @AfterAll
-    static void tearDown() {
-        postgresql.stop();
-    }
 
     @Test
     @DisplayName("should throw out of memory error")
