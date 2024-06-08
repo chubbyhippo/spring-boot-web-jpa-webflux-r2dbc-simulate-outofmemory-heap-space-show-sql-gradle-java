51540d6385dd2bfe3f332c5c3034e2b5d9d74611
chubbyhippo <mkmsacc@outlook.com>
2024-06-08 20:44:50 +0700

    Rename test package and remove unnecessary imports
    The test package has been renamed from 'com.example.demo' to 'io.github.chubbyhippo.demo'. In addition, unnecessary imports related to Testcontainers and PostgresSQL have been removed from 'TestDemoApplication.java'.

--- jpa/src/test/java/io/github/chubbyhippo/demo/TestDemoApplication.java
+++ jpa/src/test/java/io/github/chubbyhippo/demo/TestDemoApplication.java
@@ -1,12 +1,8 @@
-package com.example.demo;
+package io.github.chubbyhippo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 @ImportTestcontainers(AbstractTestcontainers.class)
