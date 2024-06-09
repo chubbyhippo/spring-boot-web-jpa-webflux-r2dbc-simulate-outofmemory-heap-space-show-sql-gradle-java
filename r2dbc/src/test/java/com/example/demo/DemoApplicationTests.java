fbe3dffcdf8dc9a57609aea8340b2996aca4de52
chubbyhippo <mkmsacc@outlook.com>
2024-06-09 18:18:10 +0700

    Refactor tests setup in DemoApplicationTests
    Removed @BeforeAll and @AfterAll annotations and methods in DemoApplicationTests class. Instead, imported TestcontainersConfiguration using @Import annotation to manage the container setup and cleanup.

--- r2dbc/src/test/java/com/example/demo/DemoApplicationTests.java
+++ r2dbc/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,27 +1,17 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class DemoApplicationTests extends AbstractTestcontainers {
+@Import(TestcontainersConfiguration.class)
+class DemoApplicationTests {
 
     @Autowired
     private WebTestClient webTestClient;
-
-    @BeforeAll
-    static void setUp() {
-        postgresql.start();
-    }
-
-    @AfterAll
-    static void tearDown() {
-        postgresql.stop();
-    }
 
     @Test
     void shouldGetStudents() {
