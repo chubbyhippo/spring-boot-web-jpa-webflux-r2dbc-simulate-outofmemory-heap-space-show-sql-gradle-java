777f178f8ff617460305aca559eb283d6b82af7f
chubbyhippo <mkmsacc@outlook.com>
2024-06-09 18:18:22 +0700

    Refactor StudentR2dbcRepositoryTest to use TestcontainersConfiguration
    StudentR2dbcRepositoryTest has been refactored to utilize TestcontainersConfiguration, removing the need for the individual setup and teardown methods for postgresql. The test has been further optimized by removing unnecessary spaces in method calls.

--- r2dbc/src/test/java/com/example/demo/infrastructure/StudentR2dbcRepositoryTest.java
+++ r2dbc/src/test/java/com/example/demo/infrastructure/StudentR2dbcRepositoryTest.java
@@ -1,35 +1,25 @@
 package com.example.demo.infrastructure;
 
-import com.example.demo.AbstractTestcontainers;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import com.example.demo.TestcontainersConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
 import reactor.test.StepVerifier;
 
 @DataR2dbcTest
-class StudentR2dbcRepositoryTest extends AbstractTestcontainers {
+@Import(TestcontainersConfiguration.class)
+class StudentR2dbcRepositoryTest {
 
     @Autowired
     private StudentR2dbcRepository studentR2dbcRepository;
-
-    @BeforeAll
-    static void setUp() {
-        postgresql.start();
-    }
-
-    @AfterAll
-    static void tearDown() {
-        postgresql.stop();
-    }
 
     @Test
     @DisplayName("should count all student records")
     void shouldCountAllStudentRecords() {
         StepVerifier.create(studentR2dbcRepository.count())
-                .expectNext( 1000000L)
+                .expectNext(1000000L)
                 .verifyComplete();
     }
 
