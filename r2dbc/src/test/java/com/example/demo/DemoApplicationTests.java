9456fea472ac37988e7fe76cde1d1006b9a10305
ch <mkmsacc@outlook.com>
2024-06-17 09:30:11 +0700

    Add Testcontainers annotation to DemoApplicationTests
    Testcontainers annotation was added to the DemoApplicationTests class. This enables the use of Testcontainers for this class, specifically for those scenarios where Docker is not disabled. This update will improve the accuracy and validity of our tests.

--- r2dbc/src/test/java/com/example/demo/DemoApplicationTests.java
+++ r2dbc/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestcontainersConfiguration.class)
+@Testcontainers(disabledWithoutDocker = true)
 class DemoApplicationTests {
 
     @Autowired
