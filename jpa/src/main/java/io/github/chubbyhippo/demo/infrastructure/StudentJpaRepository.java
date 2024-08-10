23105d4d9b651ab7bd42269000d2121589ba998e
chubbyhippo <mkmsacc@outlook.com>
2024-08-10 18:31:53 +0700

    Add native query and query hints to Student repository
    Changed the `findAllWithStream` method to use a native query. Added query hints to optimize the fetching of results, improving performance.

--- jpa/src/main/java/io/github/chubbyhippo/demo/infrastructure/StudentJpaRepository.java
+++ jpa/src/main/java/io/github/chubbyhippo/demo/infrastructure/StudentJpaRepository.java
@@ -1,7 +1,17 @@
 package io.github.chubbyhippo.demo.infrastructure;
 
 import io.github.chubbyhippo.demo.domain.Student;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+
+import java.util.stream.Stream;
 
 public interface StudentJpaRepository extends JpaRepository<Student, Long> {
+
+//    @Query("SELECT s FROM Student s")
+    @Query(value = "select * from student", nativeQuery = true)
+    @QueryHints(value = {@QueryHint(name = "org.hibernate.fetchSize", value = "10")})
+    Stream<Student> findAllWithStream();
 }
