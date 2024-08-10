6af07fa5f2aab252411e4fdcfd62eb6d2499936f
chubbyhippo <mkmsacc@outlook.com>
2024-08-10 19:14:55 +0700

    Add method to find all students as a Stream
    The `StudentRepository` interface now includes a `findAllStudents` method that returns a `Stream` of `Student` objects. This enhances the flexibility and performance when dealing with large collections of students.

--- jpa/src/main/java/io/github/chubbyhippo/demo/domain/StudentRepository.java
+++ jpa/src/main/java/io/github/chubbyhippo/demo/domain/StudentRepository.java
@@ -1,8 +1,10 @@
 package io.github.chubbyhippo.demo.domain;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface StudentRepository {
     List<Student> findAll();
     List<Student> find10Students();
+    Stream<Student> findAllStudents();
 }
