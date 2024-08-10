1113aef46906512bbe581fa040e1e9b9b50f5347
chubbyhippo <mkmsacc@outlook.com>
2024-08-10 17:09:54 +0700

    Enable Testcontainers in test configuration
    Added the @Testcontainers annotation with the `disabledWithoutDocker` property set to true in the TestDemoApplication class. This ensures Testcontainers are used in tests only when Docker is available.

--- jpa/src/test/java/io/github/chubbyhippo/demo/TestDemoApplication.java
+++ jpa/src/test/java/io/github/chubbyhippo/demo/TestDemoApplication.java
@@ -3,9 +3,11 @@ package io.github.chubbyhippo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @TestConfiguration(proxyBeanMethods = false)
 @ImportTestcontainers(TestcontainersConfiguration.class)
+@Testcontainers(disabledWithoutDocker = true)
 public class TestDemoApplication {
 
     public static void main(String[] args) {
